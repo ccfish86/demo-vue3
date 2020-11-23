ffa5bccbdc53f332d4e5cdae1b9e0447364dfb11
ccfish <ccfish@ccfish.net>
2020-11-23 15:51:39 +0800

    追加$message配置

--- element-plus/src/main.ts
+++ element-plus/src/main.ts
@@ -2,16 +2,20 @@
  * @Author: ccfish
  * @Date: 2020-11-19 11:30:33
  * @LastEditors: ccfish
- * @LastEditTime: 2020-11-20 10:39:01
+ * @LastEditTime: 2020-11-23 15:24:44
  * @Description: file content
  */
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./router";
 import installElement from "./plugins/element";
 import installIcons from "./plugins/svgicon";
 
 const app = createApp(App);
-
 installElement(app);
 installIcons(app);
-app.mount("#app");
+
+// global properties
+app.config.globalProperties.version = "1.0";
+
+app.use(router).mount("#app");
--- element-plus/src/plugins/element.ts
+++ element-plus/src/plugins/element.ts
@@ -2,14 +2,17 @@
  * @Author: ccfish
  * @Date: 2020-11-19 11:33:28
  * @LastEditors: ccfish
- * @LastEditTime: 2020-11-19 17:49:58
+ * @LastEditTime: 2020-11-23 15:49:26
  * @Description: element-plus配置
  */
 
 import ElementPlus from "element-plus";
+import Message from "element-plus/lib/message";
 import "../element-variables.scss";
 import locale from "element-plus/lib/locale/lang/zh-cn";
 
 export default (app: any) => {
   app.use(ElementPlus, { locale });
+  app.provide("$message", Message);
+  app.config.globalProperties.$message = Message;
 };
